3b3776a917a8452e2cbd0449a05bee31e67136dc
DeepeshSuryawanshi-CreArt <deepesh@creartsolutions.com>
2025-08-07 13:25:09 +0530

    Update the proejct functionality

--- OOPS/Projects/SchoolManagement.ts
+++ OOPS/Projects/SchoolManagement.ts
@@ -1,33 +1,9 @@
 // 🏗️ Project Structure:
-// 1. Person (Base class)
-// name: string
-// age: number
-// email: string
-// showDetails(): void
-// 2. Student (Inherits from Person)
-// rollNumber: string
-// grade: string
-// courses: string[]
-// enrollCourse(courseName: string): void
-// Override showDetails() to include rollNumber and grade
-// 3. Teacher (Inherits from Person)
-// employeeId: string
-// subject: string
-// assignmentsGiven: string[]
-// giveAssignment(assignment: string): void
-// Override showDetails() to include employeeId and subject
-// 4. Course
-// courseName: string
-// students: Student[]
-// teacher: Teacher | null
-// addStudent(student: Student): void
-// assignTeacher(teacher: Teacher): void
-// listStudents(): void
 
 class Person {
   name: string;
-  age: number;
-  email: string;
+  private age: number;
+  private email: string;
   constructor(name: string, age: number, email: string) {
     this.email = email;
     this.name = name;
@@ -60,9 +36,9 @@ class Student extends Person {
     this.course.push(courserName);
   }
   showDetails(): void {
-    super.showDetails()
+    super.showDetails();
     console.log(
-      `Student Roll No:${this.rollNo}  grade:${this.grade} courses:${this.course}`
+      `Student Roll No:${this.rollNo}  grade:${this.grade} courses:${this.course}\n`
     );
   }
 }
@@ -87,43 +63,127 @@ class Teacher extends Person {
   GiveAssignment(assignment: string): void {
     this.assignmentGiven.push(assignment);
   }
-  showDetails(): void {}
+  showDetails(): void {
+    super.showDetails();
+    console.log(
+      `Employe_Id:${this.employeeId}  Subject:${this.subject} Assignment:${this.assignmentGiven}\n`
+    );
+  }
 }
 
-class Course{
-    courseName:string
-    students:[Student]
-    teacher:Teacher
-    constructor(coursername:string,student:[Student],teacher:Teacher){
-        this.courseName = coursername
-        this.students = student
-        this.teacher = teacher
-    }
-    addStudent(student: Student): void{
-        this.students.push(student)
-    }
-    assignTeacher(teacher: Teacher): void{
-        this.teacher = teacher
-    }
-    listStudents(): void{
-        console.log("Student in Courser:",this.courseName)
-        this.students.forEach((student)=>console.log(`${student.showDetails()}`))
-    }
+class Course {
+  courseName: string;
+  students: [Student];
+  teacher: Teacher;
+  constructor(coursername: string, student: [Student], teacher: Teacher) {
+    this.courseName = coursername;
+    this.students = student;
+    this.teacher = teacher;
+  }
+  addStudent(student: Student): void {
+    this.students.push(student);
+  }
+  assignTeacher(teacher: Teacher): void {
+    this.teacher = teacher;
+  }
+  listStudents(): void {
+    console.log("Student in Courser:", this.courseName, "\n");
+    console.log("Courses Details show run", this.students);
+    this.students.forEach((student) => {
+      console.log(`${student.showDetails()}`);
+    });
+  }
 }
-// teachers
-let Hitesh_choudhary = new Teacher("Hitesh Cgoudhary","Hitesh.ai@hc.com",35,1101,"Web Development Javascript / Typescript",["Web Development"])
-let Strive = new Teacher("Strive","strive@gmail.com",28,1102,"Data Stracture and Algorithem",["DSA"])
-Hitesh_choudhary.GiveAssignment("Web Dev Cohot")
-Strive.GiveAssignment("DSA Sheet")
-// student
-let Dhammu = new Student("Dharmandre yadav","DhammuYadavji@youpmail.com",35,1234,60,["Data Science"])
-let Deepesh = new Student("Deepesh Suryawanshi","Deepesh.ai@Hc.com",22,1784,80,["Web Development"])
-Dhammu.enrollCourses("DSA")
-Deepesh.enrollCourses("Web Development")
 
-// coursers
-let DSA = new Course("Data Stracture and Algorithem",[Dhammu],Strive)
-let Web_dev = new Course("Web Development",[Deepesh],Strive)
-DSA.addStudent(Deepesh)
-Web_dev.listStudents()
-Deepesh.showDetails()
+const Students: Student[] = [];
+const Teachers: Teacher[] = [];
+const Courses: Course[] = [];
+
+while (true) {
+  const options = [
+    "Add Student",
+    "Add Teacher",
+    "Create Course",
+    "Enroll Student in Course",
+    "Assigne Teacher to Course",
+    "List All Course with Details",
+    "Exit-- Shut Down",
+  ];
+  console.log("Welcome to School Management System\n");
+  options.forEach((opt, index) => {
+    console.log(index + 1, `. ${opt}`);
+  });
+  let value = Number(prompt("Enter you Option:"));
+  switch (value) {
+    case 1:
+      console.log("\nAdd student");
+      let name = String(prompt("Enter the Name"));
+      let age = Number(prompt("Enter the Age"));
+      let email = String(prompt("Enter the Email"));
+      let rollNo = Number(prompt("Enter the Roll No"));
+      let grade = Number(prompt("Enter the Grad"));
+      let courseName = String(prompt("Enter the Course"));
+      const student = new Student(name, email, age, rollNo, grade, [courseName]);
+      Students.push(student);
+      console.log("\nStudent Add Successfully\n");
+      break;
+    case 2:
+      console.log("\nAdd teacher");
+      let tname = String(prompt("Enter the Name"));
+      let tage = Number(prompt("Enter the Age"));
+      let temail = String(prompt("Enter the Email"));
+      let empid = Number(prompt("Enter the Employee Id"));
+      let subject = String(prompt("Enter the Subject"));
+      let assignment = String(prompt("Enter the Course"));
+      const teacher = new Teacher(tname, temail, tage, empid, subject, [
+        assignment,
+      ]);
+      Teachers.push(teacher);
+      console.log("\nTeacher Add Successfully\n");
+      break;
+    case 3:
+      console.log("\nCreate Course");
+      let cname = String(prompt("Enter the Course Name:"));
+      Students.forEach((student, index) => {console.log(index); student.showDetails()});
+      let selectedStudent = Number(prompt("Enter the student Number"));
+      Teachers.forEach((teacher, index) => {console.log(index); teacher.showDetails()});
+      let selectedTeacher = Number(prompt("Enter the Teacher Number"));
+      // Validate student selection
+      let cstuident = Students[selectedStudent];
+      if (!cstuident) {
+        console.log("Invalid student selection!");
+        break;
+      }
+      // Validate teacher selection
+      let cteacher = Teachers[selectedTeacher];
+      if (!cteacher) {
+        console.log("Invalid teacher selection!");
+        break;
+      }
+      const course = new Course(cname, [cstuident], cteacher);
+      Courses.push(course);
+      console.log("\nCourse created successfully!\n");
+      break;
+    case 4:
+      console.log("\nEnroll student in Course");
+      break;
+    case 5:
+      console.log("\nAssigne Teacher to course");
+      break;
+    case 6:
+      console.log("\nList All Course with Details");
+      if (Courses.length === 0) {
+        console.log("No courses available.");
+      } else {
+        Courses.forEach((course, index) => {
+          console.log(`Course ${index + 1}:`);
+          course.listStudents();
+        });
+      }
+      break;
+    case 7:
+      console.log("Shut Down -- Exit");
+      break;
+  }
+  if (value === 7) break;
+}
